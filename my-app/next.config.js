ca7280a876f4705b90ee77fb1ced7161ac915431
anothercodingguy <suyash.strive@gmail.com>
2025-09-05 03:05:57 +0530

    Fix Tailwind/PostCSS setup

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      turbo: false, // disable Turbopack
-    },
-    webpack: (config) => {
-      return config; // keep using Webpack
-    },
-  };
-  
-  module.exports = nextConfig;
+  webpack: (config) => {
+    return config; // keep using Webpack
+  },
+};
 
-  
+module.exports = nextConfig;
